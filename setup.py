12f5ca2f3fb53409795021d7f92f4cb89d1c72d8
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-08-15 11:52:20 -0400

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Pyirc',
+      version='0.1.0',
+      description='Discord.py like IRC bot library',
+      author='zurgeg',
+      url='https://www.github.com/zurgeg/pyirc',
+      packages=['pyirc'],
+     )
